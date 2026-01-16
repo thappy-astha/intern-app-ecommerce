0deecdcf5e9a001a3ebb3eea5d91381b60fc2b9a
Astha Thapliyal <aasthathapliyal5@gmail.com>
2026-01-16 21:32:10 +0530

    established

--- src/main/java/com/intern/app/ecommerce/model/Admin.java
+++ src/main/java/com/intern/app/ecommerce/model/Admin.java
@@ -26,7 +26,6 @@ public class Admin {
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @JsonProperty("addressL1")
     @Column(name = "addressl1", nullable = false)
     private String addressL1;
 
@@ -36,11 +35,9 @@ public class Admin {
     @Column(name = "addressl3")
     private String addressL3;
 
-    @JsonProperty("pinCode")
     @Column(name = "pin_code", nullable = false)
     private Long pinCode;
 
-    @JsonProperty("phoneNumber")
     @Column(name = "phone_number", nullable = false)
     private Long phoneNumber;
 
