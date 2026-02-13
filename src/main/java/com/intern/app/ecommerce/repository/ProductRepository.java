9fdabac18f2ef1379ab569e853257e0725e1ef20
Astha Thapliyal <aasthathapliyal5@gmail.com>
2026-02-13 12:56:10 +0530

    established

--- src/main/java/com/intern/app/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/intern/app/ecommerce/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.intern.app.ecommerce.repository;
 import com.intern.app.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByVendorId(Long vendorId);
 }
